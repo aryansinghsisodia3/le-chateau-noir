15425b3b55b5d581246cf3ce75ad626fbcbe1998
Aryan <64405953+aryansinghsisodia3@users.noreply.github.com>
2025-02-05 13:05:42 +0530

    Delete js directory

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("nav-toggle");
-  const navList = document.getElementById("nav-list");
-
-  toggleButton.addEventListener("click", function () {
-    navList.classList.toggle("active");
-  });
-});
